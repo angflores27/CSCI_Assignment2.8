98a188be00f252e2b132c6e3ed72f272c6cf27a3
“angflores27” <angflores27@icloud.com>
2023-01-23 19:16:27 -0800

    Initial Commit

---
+++ Assignment 2.8/main.cpp
@@ -0,0 +1,25 @@
+//
+//  main.cpp
+//  Assignment 2.8
+//
+//  Created by Angela Flores on 9/28/22.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a=0;
+
+    for(int i=1;i<5;i++){
+
+           for(int j=0;j<i;j++){
+               cout<<"hello"<<endl;
+
+            }
+
+    }
+    
+    return 0;
+}
